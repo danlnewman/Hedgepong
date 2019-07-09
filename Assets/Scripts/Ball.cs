a14ff346b3a9a13393c71374ed358463315cf58a
Dan Newman <danlnewman@gmail.com>
2019-07-09 02:15:52 -0700

    Added webgl

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    Rigidbody rb;
+    public float speed;
+    float xspeed = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0.0f, -speed, 0.0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            xspeed = (transform.position.x - collision.transform.position.x) * speed;
+            rb.velocity = new Vector3(xspeed, speed, 0.0f);
+        }
+        else if (collision.transform.CompareTag("Cpu"))
+        {
+            xspeed = (transform.position.x - collision.transform.position.x) * -speed;
+            rb.velocity = new Vector3(xspeed, -speed, 0.0f);
+        }
+        else if (collision.transform.CompareTag("LeftWall"))
+        {
+            rb.velocity = new Vector3(-Mathf.Abs(xspeed), rb.velocity.y, rb.velocity.z);
+        }
+        else if (collision.transform.CompareTag("RightWall"))
+        {
+            rb.velocity = new Vector3(Mathf.Abs(xspeed), rb.velocity.y, rb.velocity.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.position = new Vector3(0f, 0f, 10f);
+        rb.velocity = new Vector3(0.0f, -speed, 0.0f);
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,7 +16,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float input = Input.GetAxis("Horizontal");
+        float input = 0f;
+        if (Input.touchCount > 0)
+        {
+            var touch = Input.touches[0];
+            if (touch.position.x < Screen.width / 2)
+            {
+                input = -1f;
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                input = 1f;
+            }
+        }
+        else
+        {
+            input = Input.GetAxis("Horizontal");
+
+        }
         if (input > 0f)
         {
             transform.Translate(speed * Time.deltaTime, 0f, 0f);
