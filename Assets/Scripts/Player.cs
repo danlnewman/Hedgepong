7f8e7b181125deb2e5780994e216327188e8c224
Dan Newman <danlnewman@gmail.com>
2019-07-09 00:40:42 -0700

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        if (input > 0f)
+        {
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        }
+        else if ( input < 0f)
+        {
+            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
